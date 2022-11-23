7a501d8c03eaa0c7c8fa50e110e0a9baa29b962d
SIDI AHMED Ahmed <sidiahmedahmed2001@gmail.com>
2022-11-23 17:03:26 +0100

    Fonction slash_cd() et mise à jour de PWD en conséquence

---
+++ cd.h
@@ -0,0 +1,12 @@
+#ifndef CD_H
+#define CD_H
+
+#include "mystring.h"
+/*
+extern struct string *OLD_PATH;
+extern struct string *PWD;
+*/
+void maj_PWD(char *ref);
+int slash_cd(char **args, int len);
+
+#endif
