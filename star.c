b70e92e24446af250a213519300a54251fb8e455
salhi younes <younessalhi520@gmail.com>
2022-12-09 20:11:22 +0100

    version finale de star

--- star.c
+++ star.c
@@ -1,18 +1,23 @@
+include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <limits.h>
 
-static void get_suffix_after_star(char *ref, int *star_index, char *result){
-	int len_result = 0, saved_star_pos = *star_index;
-	while (*(*star_index+ref) != '\0' && *(*star_index+ref) != '/'){
+static char* get_suffix_after_star(char *ref, int *star_index){
+	int len_result = -1, saved_star_pos = *star_index;
+	while (ref[*star_index] != '\0' && ref[*star_index] != '/'){
 		len_result++;
-		*star_index++;
+		*star_index = *star_index + 1;
 	}
-	*result = malloc(len_result+1);
+	char *result = malloc(len_result+1);
 	memmove(result, ref+saved_star_pos+1, len_result);
 	*(result + len_result) = '\0';
+	*star_index = *star_index - 1;
+	return result;
 }
 
 static int end_with_suffix(char *string, char *suffix){
@@ -31,9 +36,10 @@ static int end_with_suffix(char *string, char *suffix){
 
 
 static void insert_2d_array(char **result, char *data, int *current_pos){
-	*result+current_pos = malloc(strlen(data)+1);
-	memmove(*result+current_pos, data, strlen(data));
-	*(*result+current_pos) = '\0';
+	int pos = *current_pos;
+	result[pos] = malloc(strlen(data)+1);
+	memmove(result[pos], data, strlen(data));
+	result[pos][strlen(data)] = '\0';
 	*current_pos = *current_pos + 1;
 }
 
@@ -46,38 +52,40 @@ void star_aux(char *ref, char **result, int *current_pos){
 	int last_index_star = 0;
 	int contain_star = 0;
 
-	for (int index=0; *(index+ref) != '\0'; index++){
+	for (int index=0; index < strlen(ref); index++){
 		if (*(ref+index) == '/') last_index_star = index+1;
-		else if (*(ref+index) == '*' && (*((ref+index+1) == '\0') || *(ref+index+1) != '*')
+		else if (*(ref+index) == '*' && ((*(ref+index+1) == '\0') || *(ref+index+1) != '*')
 			&& ((index == 0) || *(ref+index-1) == '/')){
 				contain_star = 1;
-				get_suffix_after_star(ref, &index, &suffix);
+				suffix = get_suffix_after_star(ref, &index);
 
 				char *path = malloc(last_index_star+1);
 				memmove(path, ref, last_index_star);
 				*(path+last_index_star) = '\0';
 
 				dir = opendir(path);
-				if (dir == NULL) return;
-				while ((entry = readdir(dir)) != NULL){
-					if (strlen(entry->d_name) != 0 && end_with_suffix(entry->d_name, suffix)){
-						char *entry_path = malloc(strlen(path)+strlen(entry->d_name)+1);
-						memmove(entry_path, ref, strlen(path));
-						memmove(entry_path+strlen(path), entry->d_name, strlen(entry->d_name));
-						*(entry_path + strlen(path)+strlen(entry->d_name)) = '\0';
-						if (!lstat(entry_path, &buf)) return;
-						if (S_ISDIR(buf.mode) || S_ISLNK(buf.mode)){
-							char *updated_path = malloc(strlen(entry_path)+strlen(ref+index)+1);
-							memmove(updated_path, entry_path, strlen(entry_path));
-							memmove(updated_path + strlen(entry_path), ref+index, strlen(ref)-index);
-							*(updated_path + strlen(entry_path)+strlen(ref+index)) = '\0';
-							star_aux(updated_path, result, current_pos);
-							free(updated_path);
-						}else{
-							if (*(ref+index) != '\0') return;
-							else insert_2d_array(result, entry_path, current_pos);
+				if (dir != NULL){
+					while ((entry = readdir(dir)) != NULL){
+						if (strlen(entry->d_name) != 0 && end_with_suffix(entry->d_name, suffix)
+							&& entry->d_name[0] != '.'){
+							char *entry_path = malloc(strlen(path)+strlen(entry->d_name)+1);
+							memmove(entry_path, ref, strlen(path));
+							memmove(entry_path+strlen(path), entry->d_name, strlen(entry->d_name));
+							*(entry_path + strlen(path)+strlen(entry->d_name)) = '\0';
+							if (stat(entry_path, &buf) != 0) return;
+							if (S_ISDIR(buf.st_mode)){
+								char *updated_path = malloc(strlen(entry_path)+strlen(ref+index+1)+1);
+								memmove(updated_path, entry_path, strlen(entry_path));
+								memmove(updated_path + strlen(entry_path), ref+index+1, strlen(ref+index+1));
+								*(updated_path + strlen(entry_path)+strlen(ref+index+1)) = '\0';
+								star_aux(updated_path, result, current_pos);
+								free(updated_path);
+							}else{
+								if (index+1 >= strlen(ref))
+									insert_2d_array(result, entry_path, current_pos);
+							}
+							free(entry_path);
 						}
-						free(entry_path);
 					}
 				}
 
@@ -86,12 +94,17 @@ void star_aux(char *ref, char **result, int *current_pos){
 				free(suffix);
 		}
 	}
-	if (!contain_star) insert_2d_array(result, ref, current_pos);
+
+	if (!contain_star){
+		if (stat(ref, &buf) == 0){
+			insert_2d_array(result, ref, current_pos);
+		}
+	}
 }
 
 char** star(char *ref){
-	char **result = malloc(sizeof(char*) * PATH_MAX);
-	for (int i=0;i<PATH_MAX;i++) *result+i = NULL;
+	char **result = malloc(PATH_MAX * sizeof(char*));
+	for (int i=0;i<PATH_MAX;i++) result[i] = NULL;
 	int pos = 0;
 	star_aux(ref, result, &pos);
 	return result;
