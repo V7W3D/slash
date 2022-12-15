2b4a60fc28d8a85543f9772d6cf01baceaceca57
salhi younes <younessalhi520@gmail.com>
2022-12-15 09:33:47 +0100

    merge commit

--- util.h
+++ util.h
@@ -1,9 +1,9 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-static char* get_suffix_after_star(char *ref, int *star_index);
-static int end_with_suffix(char *string, char *suffix);
-static void insert_2d_array(char **result, char *data, int *current_pos);
+char* get_suffix_after_star(char *ref, int *star_index);
+int end_with_suffix(char *string, char *suffix);
+void insert_2d_array(char **result, char *data, int *current_pos);
 int is_intern(char *str);
 int check_args(char **args, int len);
 int is_number(char *str);
