8ada1198bc385b3576c5e92a51b0fa5059da1922
yahmiy <yanis.yahmi2002@gmail.com>
2023-01-09 00:10:56 +0100

    Fonctions redirections

--- redirections.c
+++ redirections.c
@@ -3,6 +3,37 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+
+
+// cmd < fic 
+void lecture(char ** cmd, char *fic){
+    int fd = open(fic, O_RDONLY);
+    dup2(0, fd);
+    execvp(cmd[0], cmd);
+}
+
+
+// cmd > fic 
+void sans_ecrasement_stdout(char ** cmd, char *fic){
+    int fd = open(fic,O_CREAT | O_EXCL | O_WRONLY , 0666);
+    if(fd == -1){
+        if(errno == EEXIST){
+            write(2, "le fichier existe déjà", strlen("le fichier existe déjà"));
+        }stdout_cmd_fic_avec_ecrasement
+    }
+    dup2(fd, 1);
+    execvp(cmd[0], cmd);
+    
+} 
+
+
+// cmd >| fic 
+void avec_ecrasement_stdout(char ** cmd, char *fic){
+    int fd = open(fic, O_CREAT | O_TRUNC | O_WRONLY, 0666);
+    dup2(fd, 1);
+    execvp(cmd[0],cmd);
+}
+
 // cmd >> fic
 void en_concat_stdout(char ** cmd, char * fic){
     int fd = open(fic, O_WRONLY | O_CREAT | O_APPEND, 0666);
